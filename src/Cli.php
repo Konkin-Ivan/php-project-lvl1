48c75e6e0a2b6b9698487e378a53f97a4d544108
Konkin <konkin.ivan@yandex.ru>
2022-12-06 17:52:11 +0300

    step 9

--- src/Cli.php
+++ src/Cli.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 function welcomeUser(): void
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
+    line("Welcome to the Brain Game!");
+    $name = prompt("May I have your name?");
     line("Hello, {$name}!");
 }
